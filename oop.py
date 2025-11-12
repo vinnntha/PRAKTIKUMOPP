6526065f9525d912570c73dd6dea83b0e54d28f1
vinnntha <averill.kevinatha28@gmail.com>
2025-11-12 08:39:49 +0700

    update project

--- oop.py
+++ oop.py
@@ -73,3 +73,37 @@ print("--------------------------------")
 print(armStrong(153)) # Expected: True (1^3 + 5^3 + 3^3 = 153)
 print(armStrong(370)) # Expected: True (3^3 + 7^3 + 0^3 = 370)
 print(armStrong(123)) # Expected: False
+
+
+
+class BankAccount:
+    def init(self, name):
+        self.name = name
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount <= 0:
+            return "Invalid deposit amount"
+        self.balance += amount
+        return f"Deposited {amount}. New balance: {self.balance}"
+
+    def withdraw(self, amount):
+        if amount <= 0 or amount > self.balance:
+            return "Invalid withdrawal amount or insufficient funds"
+        self.balance -= amount
+        return f"Withdrew {amount}. New balance: {self.balance}"
+
+
+# Test
+account = BankAccount("Name")
+print(account.deposit(1000))   # Deposited 1000. New balance: 1000
+print(account.withdraw(500))   # Withdrew 500. New balance: 500
+print(account.withdraw(600))   # Invalid withdrawal amount or insufficient funds
+
+
+
+    # ==== soal 8 =====
+class Student:
+    def init(self, name):
+        self.name = name
+        self.grades = []  # daftar nilai disimpan d
